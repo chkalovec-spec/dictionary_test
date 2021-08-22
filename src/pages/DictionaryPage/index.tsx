8d7849031ba348d4422f65837a6b6812a571ed37
Dmitriy <korev.dmitriy@mail.ru>
2021-08-22 22:12:50 +0300

    add dictionary page

--- src/pages/DictionaryPage/index.tsx
+++ src/pages/DictionaryPage/index.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
+
+import { DictionaryList } from '../../components/DictionaryList'
+import { DictionaryForm } from '../../components/DictionaryForm'
+import { DictionarySearch } from '../../components/DictionarySearch'
+
+import { useDictionaries } from '../../hooks/useDictionaries'
+
 export const DictionaryPage = () => {
+  const { dictionaries, deleteDictionary, addDictionary, searchDictionary } = useDictionaries()
+
+  const [isShowDictForm, setIsShowDictForm] = useState(false)
+
+  const toggleDictForm = () => {
+    setIsShowDictForm(!isShowDictForm)
+  }
+
   return (
     <>
-      <div>Dictionary page</div>
+      <h3 className='mb-4'>Список справочников: </h3>
+      <div className='mb-2 d-flex align-items-center justify-content-between'>
+        <DictionarySearch onSearchHandler={searchDictionary} />
+        <Button variant='outline-secondary' onClick={toggleDictForm}>
+          Добавить
+        </Button>
+      </div>
+      <DictionaryList dictionaries={dictionaries} onDeleteHandler={deleteDictionary} />
+      {isShowDictForm && <DictionaryForm handleClose={toggleDictForm} handleSave={addDictionary} />}
     </>
   )
 }
